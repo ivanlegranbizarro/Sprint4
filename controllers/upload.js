78b4eb5572ac6517ac943206701c0d2f868435b4
Iván Legrán <ivanlegranbizarro@gmail.com>
2022-11-29 09:12:10 +0100

    limpiando multer

--- controllers/upload.js
+++ controllers/upload.js
@@ -11,12 +11,8 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         const extension = file.originalname.split('.').pop();
-        if (extension === 'jpg' || extension === 'png' || extension === 'gif' || extension === 'jpeg') {
             const fileName = `${file.fieldname}-${Date.now()}.${extension}`;
             cb(null, fileName);
-        } else {
-            cb(new Error('File extension not allowed'));
-        }
     }
 });
 
