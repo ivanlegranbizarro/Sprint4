4af1d53271240fad587c8aa9c347f711372285f6
Iván Legrán <ivanlegranbizarro@gmail.com>
2022-11-30 09:46:04 +0100

    Fi del 4.1 tots els nivells

--- routes/time.js
+++ routes/time.js
@@ -7,12 +7,15 @@ const router = express.Router();
 Crea un endpoint /time que rebi per POST com a paràmetre un JSON amb el nom d'usuari/ària i retorni un objecte JSON que contingui l'hora i data actual. Inclogui un middleware que afegeixi la capçalera Cache-control: no-cache. Habiliti CORS (Cross-Origin Resource Sharing) en les respostes, sigui mitjançant Express o mitjançant un altre middleware.
 */
 
-router.post("/", basicAuth, (req, res) => { // TODO revisar si es pot deixar trencat per veure la validació del middleware
+router.post("/", basicAuth, (req, res) => {
     const {username} = req.body;
     const date = new Date();
 
     res.header("Cache-control", "no-cache");
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Authorization", "Basic legran 1234" + Buffer.from(username).toString("base64"));
+
+
     res.json({username, date});
 });
 
